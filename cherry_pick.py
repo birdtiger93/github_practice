39e255b72425a3c941abb7ef0a069bdbf7e59bfa
Shinbeom Choi <sbeom.choi@samsung.com>
2020-07-28 09:26:16 +0900

    cherry-pick: Add comments for readability
    Signed-off-by: Shinbeom Choi <sbeom.choi@samsung.com>

--- cherry_pick.py
+++ cherry_pick.py
@@ -6,17 +6,24 @@ def main(*argv):
     path = sys.argv[1]
     numCherryPick = int(sys.argv[2]) if len(sys.argv) > 2 else 9999
 
+    # get commit hashes from file
+    commitNumList = []
     with open(path, "r") as f:
         commits = f.read()
-    commitNumList = []
     for commit in commits.splitlines():
         commitNumList.append(commit.split(' ')[0])
+
+    # generate cherry-pick commands
     cmd = "git cherry-pick "
     cherryPickCnt = 0
     while len(commitNumList) > 0 and cherryPickCnt < numCherryPick:
         cmd += commitNumList.pop() + " "
         cherryPickCnt += 1
+
+    # show generated cherry-pick commands
     print(cmd)
+
+    # execute cherry-pick commands
     os.system(cmd)
 
 
